f331a62a85fb0654b45d9b265ba8002a57aef47b
Jon Parsons <jon.parsons@guardian.co.uk>
2015-03-12 16:11:34 +0000

    added column & row components

---
+++ grid.js
@@ -0,0 +1,12 @@
+import angular from 'angular'
+
+var grid = angular.module('grid', []);
+
+grid.directive('grid', function grid(){
+  return {
+    //your code goes here
+  };
+});
+
+export default grid;
+
