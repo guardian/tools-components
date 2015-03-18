2dbd1d18eaaa3f1618a5eb72fe63e1c23f4845b7
Jon Parsons <jon.parsons@guardian.co.uk>
2015-03-18 18:13:10 +0000

    added mixin generator

--- bin/lib/generate-mixin.js
+++ bin/lib/generate-mixin.js
@@ -1,9 +1,51 @@
-import Q from 'q';
+import Q      from 'q';
+import fs     from 'fs';
+import path   from 'path';
+import _      from 'lodash';
+import colors from 'colors';
+import mkdirp from 'mkdirp';
 
-export default (mixinName) => {
+let pReadFile   = Q.denodeify(fs.readFile);
+let pWriteFile  = Q.denodeify(fs.writeFile);
+let pMkdirP     = Q.denodeify(mkdirp);
+
+export default (mixinName, componentName) => {
 
   return Q.async(function* (){
     try {
+
+      //tell the user whats going on
+      console.log(`Generating sass/mixins/${componentName}/${mixinName}.scss`.green);
+
+      //if we dont have a directory to write to... make one.
+      let mixinDirName = path.resolve(__dirname, '../../sass/mixins/', componentName);
+      if (!fs.existsSync(mixinDirName)) {
+        yield pMkdirP(mixinDirName);
+      }
+
+      //if the mixin already exists ... bail out
+      let mixinFileName = path.resolve(mixinDirName, mixinName + '.scss');
+      if (fs.existsSync(mixinFileName)) {
+        console.log('Mixin file exists'.red);
+        process.exit(1);
+      }
+
+      //get the file template
+      var templatePath = path.resolve(__dirname, '../templates/mixin.tmpl');
+      let template = yield pReadFile(templatePath, 'utf8');
+      //compile template
+      template = _.template(template);
+      var fileData = template({
+        mixinName: mixinName
+      });
+
+      yield pWriteFile(mixinFileName, fileData, 'utf8');
+
+      console.log('-----------------------');
+      console.log(`${mixinFileName} created`.green);
+      console.log('-----------------------');
+      process.exit(0);
+
     }
     catch(e){
       console.log('-----------------------');
