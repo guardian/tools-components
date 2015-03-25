8c5f98aaf8a78c96d4c531caee68e2fb42212350
Jon Parsons <jon.parsons@guardian.co.uk>
2015-03-25 17:38:04 +0000

    finished page-label functionality

--- page-label.js
+++ page-label.js
@@ -1,4 +1,4 @@
 import pageLabel from  './lib/page-label/index';
-import css from './styles/component/page-label.css!';
+import css from './styles/components/page-label.css!';
 
 export default pageLabel;
