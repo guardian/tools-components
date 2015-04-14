cdd6b5760a9f72203a9ea4fb7198bf388de3865a
Jon Parsons <jon.parsons@guardian.co.uk>
2015-04-14 10:25:48 +0100

    update build script

--- bin/lib/release.js
+++ bin/lib/release.js
@@ -51,6 +51,9 @@ export default (releaseType) => {
       console.log(`- Checking out ${DIST_BRANCH}`.green);
       yield spawn('git', ['checkout', DIST_BRANCH]).progress(printProgress);
 
+      console.log(`- Merging ${DIST_BRANCH}`.green);
+      yield spawn('git', ['merge', DIST_BRANCH]).progress(printProgress);
+
       let ignoreFilePath = path.resolve(__dirname, '../../.gitignore');
 
       if (!fs.existsSync(ignoreFilePath)) {
